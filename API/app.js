c0fc6fc5fb74afab9f36ca846d9a4d5cc22f47b2
Marion <marion_ott@me.com>
2020-01-29 13:56:35 +0100

    [API] refactor employee router

--- API/app.js
+++ API/app.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
 
 // Express app init
-const app = express()
+const app = express();
 
 // Init bodyParser -> Request body parsing middleware
 const urlencodedParser = bodyParser.urlencoded({
-  extended: true
-})
-app.use(urlencodedParser)
-app.use(bodyParser.json())
+	extended: true
+});
+app.use(urlencodedParser);
+app.use(bodyParser.json());
 //
 
-app.use(morgan('dev'))
-app.use(cors())
+app.use(morgan('dev'));
+app.use(cors());
 
 // Router imports
-const staffRouter = require('./routes/staffRoutes')
-app.use('/api/staff', staffRouter)
+const employeeRouter = require('./routes/employeeRoutes');
+app.use('/api/employees', employeeRouter);
 
-module.exports = app
+module.exports = app;
