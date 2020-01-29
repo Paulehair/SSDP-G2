bc553595e00c1267acc7791f1ac9c22fc671e602
Paule Herman <p.herman@mazarinedigital.com>
2020-01-29 23:25:07 +0100

    [API][INFRA] add VAR ENV to connect DB + delete mongo container from docker-compose

--- API/index.js
+++ API/index.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const DB = 'mongodb://mongodb:27017/samusocial'
-const PORT = 9000
+const DB = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ssdp-9bgwa.mongodb.net/test?retryWrites=true&w=majority`;
+const PORT = 9000;
 
 // Connect to database
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  })
-  .then(() => {
-    console.log('Connected to DB')
-  })
-  .catch(e => {
-    console.log('Error while connecting to DB')
-    console.error(e)
-  })
+	.connect(DB, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true
+	})
+	.then(() => {
+		console.log('Connected to DB');
+	})
+	.catch(e => {
+		console.log('Error while connecting to DB');
+		console.error(e);
+	});
 
 // Import Express app and start server listener
 const app = require('./app');
@@ -24,6 +24,6 @@ const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 // Close server if an error is caught
 process.on('unhandledRejection', err => {
-  console.error(err.name, err.message);
-  server.close(() => process.exit(1));
+	console.error(err.name, err.message);
+	server.close(() => process.exit(1));
 });
