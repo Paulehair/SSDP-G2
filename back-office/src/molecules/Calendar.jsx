f3efbc6aec04d9eaaa733daa0c4d768de8b3d67b
Marion <marion_ott@me.com>
2020-01-30 19:42:28 +0100

    [FRONT][DEP] antdesign css cdn

--- back-office/src/molecules/Calendar.jsx
+++ back-office/src/molecules/Calendar.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { ConfigProvider } from 'antd'
 import frFR from 'antd/es/locale/fr_FR'
-import 'antd/dist/antd.css'
 import { Calendar } from 'antd'
 
 export default () => (
