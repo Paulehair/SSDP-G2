0fa047ea26aa62199ee39661ecc17ee548516216
Simon <soleausimon@hotmail.fr>
2020-01-30 11:12:08 +0100

    [BO][INFRA] remove node-sass + add hot reload to docker

--- back-office/src/App.js
+++ back-office/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import './App.scss'
+import './App.css'
 
 function App() {
-  return <div className='App'></div>
+  return <div className='App'>changes</div>
 }
 
 export default App
