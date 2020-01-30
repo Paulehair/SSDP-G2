75e4e504d1037537ccd89270d70928e0bc865409
Marion <marion_ott@me.com>
2020-01-31 00:05:08 +0100

    [API] start planning request

--- API/controllers/visitController.js
+++ API/controllers/visitController.js
@@ -3,24 +3,66 @@ const Employee = require('./../models/employeeModel');
 
 exports.getVisit = async (req, res) => {
 	try {
+		let date = new Date();
+		date.setMonth(date.getMonth() - 3);
+
 		const hotels = await Hotel.aggregate([
 			{
-				$match: {anomaly: {$gte: 45}, sector: 'Paris'}
+				$match: {
+					anomaly: {$gte: 45},
+					lastVisit: {$lt: date},
+					sector: {$eq: req.body.sector}
+				}
 			},
 			{
 				$sort: {anomaly: -1}
 			}
 		]);
-		const employees = await Employee.aggregate([
-			{
-				$match: {sector: 'Paris'}
+
+		const planning = [];
+		const daysCount = 4;
+		const mainHotels = hotels.slice(0, daysCount);
+
+		const maxVisit = 4;
+		mainHotels.forEach((hotel, i) => {
+			if (!planning[i]) {
+				planning[i] = {};
+				planning[i]['visits'] = [];
 			}
-		]);
+			planning[i]['visits'].push(hotel);
+		});
+
+		planning.forEach((day, index) => {
+			let remainingHotels = hotels.slice(daysCount * index + 1);
+
+			remainingHotels.forEach((hotel, i) => {
+				if (i < maxVisit - 1) {
+					day.visits.push(hotel);
+				}
+			});
+		});
+
+		const employees = await Employee.find({sector: req.body.sector});
+
+		const binome1 = employees.slice(0, 2);
+		const binome2 = employees.slice(2);
+
+		planning.forEach(day => {
+			day.visits.forEach((visit, i) => {
+				if (i > 1) {
+					visit['team'] = binome1;
+				} else {
+					visit['team'] = binome2;
+				}
+			});
+		});
+
 		res.status(200).json({
 			status: 'success',
 			numberOfHotels: hotels.length,
 			numberOfEmployees: employees.length,
 			data: {
+				planning,
 				hotels,
 				employees,
 				visit: {
