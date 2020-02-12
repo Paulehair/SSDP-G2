662f594548416db4a8bac5afa0ddf86d70f5149a
Paule Herman <p.herman@mazarinedigital.com>
2020-02-12 20:04:57 +0100

    [API][documentation] add apidoc & comment employee routes

--- API/routes/hotelRoutes.js
+++ API/routes/hotelRoutes.js
@@ -1,32 +1,11 @@
 const router = require('express').Router();
 const hotelController = require('./../controllers/hotelController');
 
-/* 
-  Staff route for path /api/hotels/
-  @params: none
-  @get: 
-    @returns [{}] all hotels
-  @post: 
-    create a new hotel
-    @returns {} response with confirmation
-*/
 router
 	.route('/')
 	.get(hotelController.getHotels)
 	.post(hotelController.createHotel);
 
-/* 
-  Staff route for path /api/hotels/:id
-  @params: id String
-  @get:
-    @returns {} hotel with id from param
-  @patch: 
-    update hotel with id from param
-    @returns {} response with confirmation
-  @delete:
-    delete hotel with id from param
-    @returns {} response with confirmation
-*/
 router
 	.route('/:id')
 	.get(hotelController.getHotel)
