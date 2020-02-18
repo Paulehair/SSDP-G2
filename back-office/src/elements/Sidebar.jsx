b007582b1b719f982b7a8186ffb94a8e045281c1
Marion <marion_ott@me.com>
2020-02-18 15:35:37 +0100

    [FRONT] add sector context & sector services

--- back-office/src/elements/Sidebar.jsx
+++ back-office/src/elements/Sidebar.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import styled from 'styled-components'
-import Calendar from './../molecules/Calendar'
-import Search from './../molecules/Search'
-import Area from './../molecules/Area'
+import React from 'react';
+import styled from 'styled-components';
+import Calendar from './../molecules/Calendar';
+import Search from './../molecules/Search';
+import Area from './../molecules/Area';
 
 const Sidebar = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  width: 28%;
-  max-width: 300px;
-  padding: 24px;
-  border-radius: 8px;
-  background-color: ${({ theme: {variables} }) => variables.white};
-  overflow-y: scroll;
-`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	width: 28%;
+	max-width: 300px;
+	padding: 24px;
+	border-radius: 8px;
+	background-color: ${({theme: {variables}}) => variables.white};
+	overflow-y: scroll;
+`;
 
-export default ({ zones, onChange }) => (
-  <Sidebar>
-    <Calendar />
-    <Search />
-    <Area onChange={onChange} zones={zones} />
-  </Sidebar>
-)
+export default ({sectors}) => (
+	<Sidebar>
+		<Calendar />
+		<Search />
+		<Area sectors={sectors} />
+	</Sidebar>
+);
--- back-office/src/molecules/Area.jsx
+++ back-office/src/molecules/Area.jsx
@@ -1,40 +1,54 @@
-import React from 'react'
-import styled from 'styled-components'
-import PrimaryText from './../atoms/PrimaryText'
-import SecondaryText from './../atoms/SecondaryText'
-import Input from './../atoms/Input'
-import data from '../data/data'
+import React, {useContext} from 'react';
+import styled from 'styled-components';
+import SectorContext from './../context/SectorContext';
+import PrimaryText from './../atoms/PrimaryText';
+import SecondaryText from './../atoms/SecondaryText';
+import Input from './../atoms/Input';
+import data from '../data/data';
 
 const Area = styled.div`
-  div {
-    display: flex;
-    align-items: center;
-    margin-bottom: 5px;
-  }
-  .areaTitle {
-    margin-bottom: 10px;
-  }
-`
+	div {
+		display: flex;
+		align-items: center;
+		margin-bottom: 5px;
+	}
+	.areaTitle {
+		margin-bottom: 10px;
+	}
+`;
 
-export default ({ zones, onChange }) => {
-  const inputData = {
-    ...data.input.zone,
-    onChange,
-    placeholder: data.input.zone.name,
-    value: data.input.zone.name
-  }
+export default ({sectors}) => {
+	const {currentSector, toggleSector} = useContext(SectorContext);
 
-  return (
-    <Area>
-      <div className='areaTitle'>
-        <PrimaryText text='Secteurs' />
-      </div>
-      {zones.map((zone, i) => (
-        <div key={i}>
-          <Input data={{...inputData, value: zone.code, checked: zone.active}} />
-          <SecondaryText text={`Secteur du ${zone.code}`} black={true} />
-        </div>
-      ))}
-    </Area>
-  )
-}
+	const handleChange = event => {
+		const id = event.target.value;
+		toggleSector(id);
+	};
+
+	const inputData = {
+		...data.input.zone,
+		onChange: handleChange,
+		placeholder: data.input.zone.name,
+		value: data.input.zone.name
+	};
+
+	return (
+		<Area>
+			<div className="areaTitle">
+				<PrimaryText text="Secteurs" />
+			</div>
+			{sectors.map((sector, i) => (
+				<div key={i}>
+					<Input
+						data={{
+							...inputData,
+							value: sector._id,
+							checked: currentSector === sector._id ? true : false
+						}}
+					/>
+					<SecondaryText text={`Secteur du ${sector.zone}`} black={true} />
+				</div>
+			))}
+		</Area>
+	);
+};
