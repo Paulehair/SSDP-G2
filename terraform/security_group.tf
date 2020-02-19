58cd2e73c30a8385ae1790465098f40a97ead573
Paule Herman <p.herman@mazarinedigital.com>
2020-02-19 15:40:38 +0100

    [infra][ansible] add ansible files

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -8,6 +8,22 @@ resource "aws_default_security_group" "default" {
     description = "HETIC IP"
     cidr_blocks = ["78.31.41.54/32"]
   }
+  ingress {
+    # TLS
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    description = "HETIC IP"
+    cidr_blocks = ["78.31.41.54/32"]
+  }
+  ingress {
+    # TLS
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    description = "HETIC IP"
+    cidr_blocks = ["78.31.41.54/32"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
